119e824f73b29edb6cf9e1584f59abe57d94b532
Avish Kumar <80064807+avishmonga@users.noreply.github.com>
2022-04-24 19:15:56 +0530

    done

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose")
 
-
 module.exports = ()=>{
    return mongoose.connect("mongodb+srv://Avish:XWuQfdBAvHPDUXIW@cluster0.ahjtx.mongodb.net/cointab")
 }
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const app = require("./index");
 const connect = require("./config/db");
-app.listen(2345, async () => {
+require("dotenv").config()
+let PORT = process.env.PORT || 2345
+app.listen(PORT, async () => {
   try {
     await connect();
 
